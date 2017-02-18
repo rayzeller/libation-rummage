8c0162a8f3bae3528bcc3bce3d5c5f45b45657cc
Ray Zeller <rayzeller@gmail.com>
2017-02-18 11:50:10 -0700

    Move libation queries to end of code block for accurate results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
   before_filter :filter_params
 
   def index
-    @queries = Libation.group(:query).order("count_all desc").count
     if params[:q]
       @result = Libation.search_and_log(params[:q]).data
     else
       @result = []
     end
+    @queries = Libation.group(:query).order("count_all desc").count
   end
 
   private
